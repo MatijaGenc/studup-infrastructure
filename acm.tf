3f7cd366aa602800ba256249276607fb419eb2aa
Daniel Tosic <daniel@example.com>
2026-07-26 09:49:07 +0200

    S-027: Fix ACM validation record syntax (tolist for set type)

--- acm.tf
+++ acm.tf
@@ -7,6 +7,28 @@ resource "aws_acm_certificate" "studup_wildcard" {
   }
 }
 
+resource "aws_acm_certificate" "studup_root" {
+  domain_name       = "studup.net"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "studup_root_cert_validation" {
+  zone_id = aws_route53_zone.studup.zone_id
+  name    = tolist(aws_acm_certificate.studup_root.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.studup_root.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.studup_root.domain_validation_options)[0].resource_record_value]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "studup_root" {
+  certificate_arn         = aws_acm_certificate.studup_root.arn
+  validation_record_fqdns = [aws_route53_record.studup_root_cert_validation.fqdn]
+}
+
 resource "aws_acm_certificate" "www_studup" {
   provider          = aws.us-east-1
   domain_name       = "www.studup.net"
