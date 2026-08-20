037189c4de5e14c9e6c8f47ca72a74041197bbf5
Daniel Tosic <daniel@example.com>
2026-08-21 00:02:34 +0200

    A-019: Add route_table_ids to S3 VPC Gateway Endpoint

--- data.tf
+++ data.tf
@@ -22,7 +22,12 @@ locals {
   subnet_ids = [for s in data.aws_subnet.default : s.id]
 }
 
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = data.aws_vpc.default.id
-  service_name = "com.amazonaws.${data.aws_region.current.region}.s3"
+  vpc_id          = data.aws_vpc.default.id
+  service_name    = "com.amazonaws.${data.aws_region.current.region}.s3"
+  route_table_ids = [data.aws_route_table.default.id]
 }
