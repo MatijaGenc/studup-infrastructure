af8986ee5d6583b496d9e9312fbaec53268f3b05
Daniel Tosic <daniel@example.com>
2026-08-31 23:01:22 +0200

    A-088: Increase budget to realistic value /month - multi-AZ RDS alone costs -30/month

--- budget.tf
+++ budget.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "monthly" {
   name         = "studup-monthly-budget"
   budget_type  = "COST"
-  limit_amount = "30"
+  limit_amount = "80"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
