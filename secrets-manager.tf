6b0a681c0be773b640e23f4320254f7d9424e349
Daniel Tosic <daniel@example.com>
2026-08-31 14:13:19 +0200

    A-074: Add VAPID secret version resources - populate vapid_public_key and vapid_private_key secrets with values from variables

--- secrets-manager.tf
+++ secrets-manager.tf
@@ -45,7 +45,17 @@ resource "aws_secretsmanager_secret" "vapid_public_key" {
   description = "StudUp Web Push VAPID public key"
 }
 
+resource "aws_secretsmanager_secret_version" "vapid_public_key" {
+  secret_id     = aws_secretsmanager_secret.vapid_public_key.id
+  secret_string = var.vapid_public_key
+}
+
 resource "aws_secretsmanager_secret" "vapid_private_key" {
   name        = "studup-vapid-private-key"
   description = "StudUp Web Push VAPID private key"
+}
+
+resource "aws_secretsmanager_secret_version" "vapid_private_key" {
+  secret_id     = aws_secretsmanager_secret.vapid_private_key.id
+  secret_string = var.vapid_private_key
 }
